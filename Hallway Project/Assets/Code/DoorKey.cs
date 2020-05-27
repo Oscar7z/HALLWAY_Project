2b4c79e9bed857a5cad8b5cf7de3ce775a0f7c3c
Oscar7z <42544170+Oscar7z@users.noreply.github.com>
2020-05-26 20:45:48 -0500

    Door and Key Interaction

---
+++ Hallway Project/Assets/Code/DoorKey.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKey : MonoBehaviour
+{
+    public bool inTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inTrigger = true;
+        }
+            
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DoorScript.doorKey = true;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (inTrigger)
+        {
+            GUI.Box(new Rect(0, 69, 200, 25),"Press E to take key");
+        }
+    }
+}
--- Hallway Project/Assets/Code/DoorScript.cs
+++ Hallway Project/Assets/Code/DoorScript.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool doorKey;
+    public bool open;
+    public bool close;
+    public bool inTrigger;
+
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            inTrigger = true;
+        }
     }
 
-    // Update is called once per frame
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
     void Update()
     {
-        
+
+        if (inTrigger)
+        {
+            if (close)
+            {
+                if (doorKey)
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        open = true;
+                        close = false;
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    open = false;
+                    close = true;
+                }
+            }
+        }
+
+        if (open)
+        {
+            var newRotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -90f, 0.0f), Time.deltaTime * 10);
+            transform.rotation = newRotation;
+        }
+        else
+        {
+            var newRotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 10);
+            transform.rotation = newRotation;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (open)
+        {
+            GUI.Box(new Rect(0, 0, 250, 25),"Press E to close");
+        }
+        else
+        {
+            if (inTrigger && doorKey)
+            {
+                GUI.Box(new Rect(0, 0, 250, 25), "Press E to open");
+            }
+            else if (inTrigger)
+            {
+                GUI.Box(new Rect(0, 0, 250, 25), "Need Key");
+            }
+        }
     }
 }
