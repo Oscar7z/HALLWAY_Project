4f8ab777894827e2e3d1e849d6b90f1a135a6275
Oscar7z <42544170+Oscar7z@users.noreply.github.com>
2020-05-26 21:56:08 -0500

    Enemy added

---
+++ Hallway Project/Assets/Code/Chaser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chaser : MonoBehaviour
+{
+    private NavMeshAgent chaser;
+
+    public GameObject Player;
+
+    public float chaserDistanceRun = 4.0f;
+    void Start()
+    {
+        chaser = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        if (distance < chaserDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            chaser.SetDestination(newPos);
+        }
+
+    }
+}
